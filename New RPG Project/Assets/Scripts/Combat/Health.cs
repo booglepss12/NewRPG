a57beab69f591e80a33f80eb66fb92c14e525bab
Unknown <grooney4@gmail.com>
2019-03-17 11:11:37 -0400

    fixed script so that player stops attacking after enemy death

--- New RPG Project/Assets/Scripts/Combat/Health.cs
+++ New RPG Project/Assets/Scripts/Combat/Health.cs
@@ -1,23 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-namespace RPG.Combat{
+﻿using UnityEngine;
+
+namespace RPG.Combat
+{
     public class Health : MonoBehaviour
     {
-        [SerializeField]float healthPoints = 100f;
+        [SerializeField] float healthPoints = 100f;
+
         bool isDead = false;
-        public void TakeDamage(float damage){
+
+        public bool IsDead()
+        {
+            return isDead;
+        }
+
+        public void TakeDamage(float damage)
+        {
             healthPoints = Mathf.Max(healthPoints - damage, 0);
-            if(healthPoints==0){
+            if (healthPoints == 0)
+            {
                 Die();
             }
-
         }
-        public void Die(){
+
+        private void Die()
+        {
             if (isDead) return;
+
             isDead = true;
-                GetComponent<Animator>().SetTrigger("die");
-           
+            GetComponent<Animator>().SetTrigger("die");
         }
     }
 }
